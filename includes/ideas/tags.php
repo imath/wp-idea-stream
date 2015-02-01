e0d75578726fe206bcecbcf527d8664150d999f9
imath <math.viet@gmail.com>
2015-02-01 16:55:04 +0100

    Use display name in idea footer fixes #3

--- includes/ideas/tags.php
+++ includes/ideas/tags.php
@@ -1353,9 +1353,9 @@ function wp_idea_stream_ideas_the_idea_footer() {
 			// no point at the end
 			$retarray['date'] = sprintf( _x( 'on %s', 'idea date of publication no point', 'wp-idea-stream' ), $date );
 
-			$username = wp_idea_stream_users_get_user_data( 'id', $idea->post_author, 'user_nicename' );
-			$user_link = '<a class="idea-author" href="' . esc_url( wp_idea_stream_users_get_user_profile_url( $idea->post_author, $username ) ) . '" title="' . esc_attr( $username ) . '">';
-			$user_link .= get_avatar( $idea->post_author, 20 ) . esc_html( $username ) . '</a>';
+			$user = wp_idea_stream_users_get_user_data( 'id', $idea->post_author );
+			$user_link = '<a class="idea-author" href="' . esc_url( wp_idea_stream_users_get_user_profile_url( $idea->post_author, $user->user_nicename ) ) . '" title="' . esc_attr( $user->display_name ) . '">';
+			$user_link .= get_avatar( $idea->post_author, 20 ) . esc_html( $user->display_name ) . '</a>';
 
 			$retarray['author'] = sprintf( _x( 'by %s.', 'single idea author link', 'wp-idea-stream' ), $user_link );
 		}
